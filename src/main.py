d6790c44c9407e2e4d9134519f2b2d546562c4b0
Code_Krosis <xavierivan2000@hotmail.com>
2019-10-19 23:48:04 -0500

    Implementacion de cambios entre Frames a traves de clases, el llamado de imagenes cambio un poco para hacerlo funcionar

---
+++ src/main.py
@@ -0,0 +1,97 @@
+import tkinter as tk
+
+#Clase base que controla los frames, recursos y switches del programa
+class SampleApp(tk.Tk):
+    def __init__(self):
+        #setea tkinter
+        tk.Tk.__init__(self)
+        self._frame = None
+        self.switch_frame(MainMenu) #frame por defecto
+
+    def switch_frame(self, frame_class):
+        #Destruye el frame actual y lo reemplaza con uno nuevo
+        new_frame = frame_class(self)#setea el nuevo frame
+        if self._frame is not None:
+            self._frame.destroy()#destruye el actual
+        #invoca el frame en pantalla
+        self._frame = new_frame
+        self._frame.pack()
+
+#Cada subclase representa un subframde del programa
+class MainMenu(tk.Frame):
+    def __init__(self, master):
+        #setup del frame y recursos
+        self.master = master
+        self.master.title("Aerolinea Arthur Ryan -MenuPrincipal")
+        logo = tk.PhotoImage(file='../logo_FISC.png')
+        bgMap = tk.PhotoImage(file="../Mapa_de_Panama_1.png")  # archivo fuente de la imagen del background
+
+        self.master.tk.call('wm','iconphoto', self.master._w, logo)
+        tk.Frame.__init__(self, self.master)
+        self.canvas = tk.Canvas(self, width=1200, height=500, bg='black')
+        self.canvas.pack()
+        #dibujar bg
+        bgLabel = tk.Label(self, image=bgMap)
+        bgLabel.image = bgMap
+        bgLabel.place(x=0, y=0)
+        #Despues de aqui, pon todos los objectos q quieras y recuerda hacer el llamado para hacer el cambio
+        self.BotonesMapa()
+        # tk.Label(self, text="This is the start page").pack(side="top", fill="x", pady=10)
+        # tk.Button(self, text="Open page one",
+        #           command=lambda: master.switch_frame(PageOne)).pack()
+        # tk.Button(self, text="Open page two",
+        #           command=lambda: master.switch_frame(PageTwo)).pack()
+
+    def BotonesMapa(self):
+        boc = tk.Button(self.master, text="Bocas del toro", command=lambda: self.master.switch_frame(PageOne))
+        boc.place(x=40, y=75)
+        coc = tk.Button(self.master, text="Coclé")
+        coc.place(x=510, y=230)
+        col = tk.Button(self.master, text="Colón")
+        col.place(x=500, y=130)
+        chi = tk.Button(self.master, text="Chiriquí")
+        chi.place(x=80, y=230)
+        dar = tk.Button(self.master, text="Darién")
+        dar.place(x=1000, y=290)
+        her = tk.Button(self.master, text="Herrera")
+        her.place(x=470, y=350)
+        ls = tk.Button(self.master, text="Los Santos")
+        ls.place(x=510, y=450)
+        pnm = tk.Button(self.master, text="Panamá")
+        pnm.place(x=730, y=100)
+        ver = tk.Button(self.master, text="Veraguas")
+        ver.place(x=340, y=300)
+        gy = tk.Button(self.master, text="Guna Yala")
+        gy.place(x=980, y=80)
+        ewn = tk.Button(self.master, text="Emberá\n Wounaan")
+        ewn.place(x=1100, y=250)
+        nb = tk.Button(self.master, text="Ngobe Bugle")
+        nb.place(x=220, y=190)
+        pnmo = tk.Button(self.master, text="Panamá\n Oeste")
+        pnmo.place(x=620, y=160)
+
+
+
+
+#Esta frame se activa al seleccionar un Lugar del mapa
+class PageOne(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        canvas = tk.Canvas(self, width=1200, height=700)
+        canvas.pack()
+        tk.Label(self, text="This is page one").pack(side="top", fill="x", pady=10)
+        tk.Button(self, text="Return to start page",
+                  command=lambda: master.switch_frame(MainMenu)).pack()
+
+class PageTwo(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        canvas = tk.Canvas(self, width=100, height=200)
+        canvas.pack()
+        tk.Label(self, text="This is page two").pack(side="top", fill="x", pady=10)
+        tk.Button(self, text="Return to start page",
+                  command=lambda: master.switch_frame(MainMenu)).pack()
+
+if __name__ == "__main__":
+    app = SampleApp()
+    app.mainloop()

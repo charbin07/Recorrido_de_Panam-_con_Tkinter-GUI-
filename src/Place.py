d06b5a781d552e481848928d630944a35b7a3e14
Code_Krosis <xavierivan2000@hotmail.com>
2019-10-21 11:44:10 -0500

    Se agregaron nuevas carpetas con descripciones e imagenes

--- src/Place.py
+++ src/Place.py
@@ -1,7 +1,6 @@
 
 
 class Place():
-
     def __init__(self, name, src, imgsrc):
         self.name = name
         self.description = self.getFileInfo(src)
@@ -11,6 +10,7 @@ class Place():
     def getFileInfo(self, src):
         try:
             with open(src) as f:
+                # for line in f.readlines():
                 text = f.read()
                 print(f"Archivo Leido file:{src}")
         except FileNotFoundError:
@@ -27,8 +27,22 @@ class Place():
     def getDescription(self):
         return self.description
 
+#provincias
+LosSantos = Place("Los Santos", "../res/provincias/LosSantos/LosSantos.txt", "../res/provincias/LosSantos/LosSantos-mapa.png")
+Herrera = Place("Herrera", "../res/provincias/Herrera/Herrera.txt", "../res/provincias/Herrera/Herrera-mapa.png")
+Darien = Place("Darien", "../res/provincias/Darien/Darien.txt", "../res/provincias/Darien/darien-Mapa.png")
+Panama = Place("Panama", "../res/provincias/Panama/panama.txt", "../res/provincias/Panama/panama.png")
+PanamaOeste = Place("Panama Oeste", "../res/provincias/PanamaOeste/panamaOeste.txt", "../res/provincias/PanamaOeste/panamaOeste-mapa.png")
+Veraguas = Place("Veraguas", "../res/provincias/Veraguas/veraguas.txt", "../res/provincias/Veraguas/veraguas-mapa.png")
 
-LosSantos = Place("Los Santos", "../res/LosSantos.txt", "../res/LosSantos-mapa.png")
-Herrera = Place("Herrera", "../res/Herrera.txt", "../res/Herrera-mapa.png")
-Darien = Place("Darien", "../res/Darien.txt", "../res/darien-Mapa.png")
+#comarcas
+Embera = Place("Embera", "../res/comarcas/Embera/comarca-Embera.txt", "../res/comarcas/Embera/Embera.png")
+Guna = Place("Guna Yala", "../res/comarcas/GunaYala/Guna_yala.txt", "../res/comarcas/GunaYala/comarca_Guna.png")
+Ngobe = Place("Ngobe-Bugle", "../res/comarcas/Ngobe-Bugle/Ngobe-Bugle.txt", "../res/comarcas/Ngobe-Bugle/Ngobe-Bugle.png")
+
+
+
+
+
+
 
